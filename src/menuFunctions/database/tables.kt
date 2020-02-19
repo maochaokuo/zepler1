63d994c7803c628103438ff775d1be562ea29b53
Thomas Mao <tm1813@gmail.com>
2020-02-19 22:58:35 +0800

    add Beeper event listener sample

--- src/menuFunctions/database/tables.kt
+++ src/menuFunctions/database/tables.kt
@@ -3,8 +3,13 @@ package menuFunctions.database
 import menuFunctions.ActionBase
 import javax.swing.*
 
-class tables(frm: JFrame,
- override val onActionListenerListener: OnActionListener)
+public open class BaseFrame(frm: JFrame,
+         override val onActionListenerListener: OnActionListener)
     : ActionBase(frm, onActionListenerListener) {
+
+}
+
+public class tables(frm: JFrame, onActionListenerListener: OnActionListener)
+    : BaseFrame(frm, onActionListenerListener){
 
 }
