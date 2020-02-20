112730d56d1deb49fe0467125935c5639cf49f39
Thomas Mao <tm1813@gmail.com>
2020-02-20 23:08:05 +0800

    mouse related event handlers

--- src/javas/event/ListSelectionDemo.java
+++ src/javas/event/ListSelectionDemo.java
@@ -29,7 +29,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */ 
 
-package javas;
+package javas.event;
 
 /*
  * ListSelectionDemo.java requires no other files.
@@ -37,9 +37,7 @@ package javas;
 
 import javax.swing.*;
 import javax.swing.event.*;
-import javax.swing.table.*;
 
-import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 
